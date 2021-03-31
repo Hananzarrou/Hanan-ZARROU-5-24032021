d4688d3f44f53f3797c82dd8bc2b444b818caea9
hanan <hananzarrou@gmail.com>
2021-03-31 12:33:21 +0200

    add js camera

---
+++ Front_end/JS/camera.js
@@ -0,0 +1,106 @@
+const urlCamera = new URLSearchParams(window.location.search);
+const idCamera = urlCamera.get("id");
+
+
+
+
+
+function getCamera(data, idCamera) {
+
+    let myCamera = data.find(data => data["id"] == idCamera);
+
+
+}
+
+fetch('http://localhost:3000/api/cameras/' + idCamera)
+.then(res => res.json())
+.then(myCamera => {
+
+    
+    let section = document.querySelector('.camera');
+
+
+    let camera = document.createElement('div');
+    camera.setAttribute("class", 'picture');
+    section.appendChild(camera);
+
+    
+    let image = document.createElement('img');
+    image.setAttribute("class", 'image2');
+    image.src = myCamera.imageUrl;
+    camera.appendChild(image);
+
+    let nom = document.createElement('p');
+    nom.setAttribute("class", 'name2');
+    nom.textContent = myCamera.name;
+    camera.appendChild(nom);
+
+    let prix = document.createElement('p');
+    prix.setAttribute("class", 'price2');
+    prix.textContent = myCamera.price / 100 + ' €';
+    camera.appendChild(prix);
+
+    let description = document.createElement('p');
+    description.setAttribute("class", 'description');
+    description.textContent = myCamera.description;
+    camera.appendChild(description);
+    
+
+    let lentille = document.createElement('select');
+    lentille.setAttribute("id", 'liste');
+    camera.appendChild(lentille);
+    let myLenses = myCamera.lenses;
+
+
+    for(let i = 0; i < myLenses.length; i++) {
+        let el = document.createElement("option");
+        el.textContent = myLenses[i];
+        liste.appendChild(el);
+
+    }
+
+    let boutton = document.createElement('button');
+    boutton.setAttribute("class", 'btn-produit2');
+    boutton.textContent = `Ajouter au Panier`;
+    camera.appendChild(boutton);
+
+
+    boutton.addEventListener('click', (e) => {
+          
+        const produit = {
+
+            id: myCamera._id,
+            name: myCamera.name,
+            price: myCamera.price / 100,
+            image: myCamera.imageUrl,
+        
+        };
+
+        let products = JSON.parse(localStorage.getItem("panier"))
+        
+        if(products) {
+
+            products.push(produit)
+            localStorage.setItem("panier", JSON.stringify(products))
+
+        }
+        else {
+
+            let produitList = []
+            produitList.push(produit)
+            localStorage.setItem("panier", JSON.stringify(produitList))
+        }
+
+        e.preventDefault()
+        
+        alert("Votre produit a été ajoutée au panier");
+
+        window.location.href = 'panier.html';
+         
+    });
+    console.log(myCamera);
+
+
+})
+
+.catch(error => alert("Erreur : " + error));
---
+++ Front_end/JS/commande.js
@@ -0,0 +1,203 @@
+let monPanier = localStorage.getItem("panier");
+let panierTotal = JSON.parse(monPanier);
+
+
+
+// PANIER //
+
+for(let i = 0; i < panierTotal.length; i++) {
+
+    let monTableau = document.getElementById("table");
+    let ligne = monTableau.insertRow();
+    tbody.appendChild(ligne);
+
+    let cell = ligne.insertCell(0);
+    ligne.appendChild(cell);
+    cell.textContent = panierTotal[i].name;
+
+    let cell1 = ligne.insertCell(1);
+    ligne.appendChild(cell1);
+    let boutton = document.createElement("button");
+    boutton.setAttribute("id", panierTotal[i].id);
+
+
+    cell1.appendChild(boutton);
+    boutton.textContent = "Supprimer";
+
+    let cell2 = ligne.insertCell(2);
+    ligne.appendChild(cell1);
+    cell2.textContent = panierTotal[i].price + " €";
+
+
+    boutton.addEventListener("click", function() {
+        panierTotal.splice(i)
+        localStorage.setItem("panier", JSON.stringify(panierTotal))
+        window.location.reload()
+
+
+    });
+};
+
+let prixTotal = [];
+
+for (let m = 0; m < panierTotal.length; m++) {
+    let prixProduit = panierTotal[m].price;
+
+    prixTotal.push(prixProduit)
+
+    console.log(prixTotal);
+
+}
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+const prix = prixTotal.reduce(reducer, 0);
+console.log(prix);
+
+const affichage = `<th class= "prixTotal">${prix} €</th>`
+
+
+thead2.insertAdjacentHTML("beforeend", affichage);
+
+
+// FORMULAIRE //
+
+document.getElementById("formulaire").addEventListener("submit", function(e) {
+
+let erreur;
+let prenom = document.getElementById("prenom");
+let firstname = /[a-zA-ZÀ-ÿ]/;
+
+let nom = document.getElementById("nom");
+let name = /[a-zA-Z]/;
+
+let adresse = document.getElementById("adresse");
+let boxmail = /[a-zA-Z0-9_]/;
+
+let ville = document.getElementById("ville");
+let city = /[a-zA-Z]/;
+
+let mail = document.getElementById("mail");
+let email = /[a-z0-9._%+-]+@[a-z0-9.-]+[.][a-z]{2,4}/;
+
+
+
+
+
+    if(ville.value == "") {
+
+        erreur = "Entrez votre ville svp";
+
+    } else if (!city.test(ville)) {
+
+        erreur = "Ceci n'est pas le nom d'une ville";
+
+
+    }
+
+    if(mail.value == "") {
+
+        erreur = "Entrez votre adresse mail svp";
+
+
+    } else if (!email.test(mail.value)) {
+
+        erreur = "Ceci n'est pas un adresse mail valide";
+
+
+    }
+
+    if(adresse.value == "") {
+
+        erreur = "Entrez votre adresse postale svp";
+
+    } else if (!boxmail.test(adresse.value)) {
+
+        erreur = "Ceci n'est pas un adresse postale";
+
+    }
+
+    if(nom.value == "") {
+
+        erreur = "Entrez votre nom de famille svp";
+    }
+
+    else if (!name.test(nom.value))  {
+
+        erreur = "Ceci n'est pas un nom de famille";
+
+
+    }
+
+    if(prenom.value == "") {
+
+        erreur = "Entrez votre prénom svp";
+
+
+    } else if (!firstname.test(prenom.value)) {
+
+        erreur = "Ceci n'est pas un prénom";
+    }
+
+    if(erreur) {
+
+        e.preventDefault();
+        document.getElementById("erreur").innerHTML = erreur;
+
+
+    }
+    else {
+
+        alert('PAYMENT PRÊT À ÊTRE ENVOYÉ');
+
+    }
+
+    let contact = {
+
+        firstName: prenom.value,
+        lastName: nom.value,
+        address: adresse.value,
+        city: ville.value,
+        mail: mail.value,
+
+    };
+
+    localStorage.setItem("contact", JSON.stringify(contact));
+
+
+    let products = [];
+
+    for(let i = 0; i < panierTotal.length; i++) {
+        products.push(panierTotal[i].id);
+        localStorage.setItem("id", JSON.stringify(products));
+
+    };
+
+    const request = {
+
+        contact : contact,
+        products : products,
+
+    }
+
+ fetch('http://localhost:3000/api/cameras/order', {
+
+        method: "POST",
+        headers: {
+
+            "Content-Type": "application/json"
+
+        },
+
+        body: JSON.stringify(request)
+
+    })
+
+    .then(response => response.json())
+    .then(() => {
+
+        window.location.href = 'validation.html';
+    })
+
+    .catch(err => console.log("erreur : " + response.status))
+
+});

7da12e2bed22183ca6f9dc8ff14d053dc4f46970
hanan <hananzarrou@gmail.com>
2021-04-10 16:30:43 +0200

    file confirmation

--- Front_end/JS/confirmation.js
+++ Front_end/JS/confirmation.js
@@ -5,16 +5,47 @@ let commande = localStorage.getItem("id");
 let identite = JSON.parse(commande);
 console.log(identite)
 
+let contact = localStorage.getItem("validation");
+console.log(contact)
+
 
 let div = document.createElement('div');
 div.id = "div1";
+
+let validationCommande = document.getElementById("validation")
+//validationCommande.textContent = identite;//
+//let validation = document.getElementById("div1");//
+validationCommande.appendChild(div);
+
+let p = document.createElement("p");
+div.appendChild(p);
+p.textContent = "Votre récapitulatif de votre commande"
+
+let pcontact = document.createElement("p");
+div.appendChild(pcontact);
+pcontact.textContent = contact;
+
+let paragraphe = document.createElement("p");
+div.appendChild(paragraphe);
+paragraphe.textContent = "Numéro de votre commande"
+
+
 let validationCommande = document.createElement("p")
 validationCommande.textContent = identite;
 let validation = document.getElementById("div1");
 validation.appendChild(validationCommande);
 
+let montant = document.createElement("p");
+div.appendChild(montant);
+montant.textContent = "Montant total de votre commande"
+
+
+let validationTotal = document.createElement("p")
+validationTotal.textContent = resultat + " €";
+
+validation.appendChild(validationTotal);
 // remplacement des variable
-let HTMLOrder = document.getElementById("div1")
+/*let HTMLOrder = document.getElementById("div1")
 let myHTMLOrder = ""
 
 let HTMLArticles = document.getElementById("div1")
@@ -28,7 +59,7 @@ Nous vous remercions
   ${contact.firstName} ${contact.lastName} pour votre commande. Voici l'identifiant de votre achat : <br></h1>
   <p>Votre Commande a été effectuée avec succes <br>Vous recevrez votre commande dans les plus bref délais </p>`
 
-HTMLOrder.innerHTML = myHTMLOrder
+HTMLOrder.innerHTML = myHTMLOrder*/
 
 
 
